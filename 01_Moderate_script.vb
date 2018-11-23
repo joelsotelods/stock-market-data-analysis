4943e5ad87e3d617c36e7eb962cbae3c2c429709
Joel Sotelo <joel.pilgrim88@gmail.com>
2018-11-23 01:57:08 -0600

    Starting to solve the Moderate version of the Assignment

--- 01_Moderate_script.vb
+++ 01_Moderate_script.vb
@@ -9,13 +9,23 @@ Sub stock_market_data_analysis()
 
     ticker_index = 2
 
+    'Varialbes to store the yearly change from open to close
+    Dim open_val As Double
+    Dim close_val As Double
+
     For Each currentsheet In ThisWorkbook.Worksheets
 
         SheetNameStr = currentsheet.Name
         CodeNameStr = currentsheet.CodeName
 
+        ' temp conditional
+        if SheetNameStr = "2015" then Exit For
+            
+
         currentsheet.Range("I1").Value = "Ticker"
-        currentsheet.Range("J1").Value = "Total Stock Volume"
+        currentsheet.Range("J1").Value = "Yearly Change"
+        currentsheet.Range("K1").Value = "Percent Change"
+        currentsheet.Range("L1").Value = "Total Stock Volume"
 
         LastRow = currentsheet.Cells(currentsheet.Rows.Count, 1).End(xlUp).Row
                                   
@@ -24,11 +34,18 @@ Sub stock_market_data_analysis()
             
             if IsEmpty(currentsheet.Cells(ticker_index, 9).Value)  then
                 currentsheet.Cells(ticker_index, 9).Value = currentsheet.Cells(i, 1).Value
-                currentsheet.Cells(ticker_index, 10).Value = currentsheet.Cells(i, 7).Value
+                currentsheet.Cells(ticker_index, 12).Value = currentsheet.Cells(i, 7).Value
+                
+                'xx
+                open_val = currentsheet.Cells(i, 3).Value
             else
                 if currentsheet.Cells(ticker_index, 9).Value = currentsheet.Cells(i, 1).Value then
-                    currentsheet.Cells(ticker_index, 10).Value = currentsheet.Cells(ticker_index, 10).Value + currentsheet.Cells(i, 7).Value
+                    currentsheet.Cells(ticker_index, 12).Value = currentsheet.Cells(ticker_index, 12).Value + currentsheet.Cells(i, 7).Value
                 else
+                    'save the close val of the year (last row of the Ticker)
+                    close_val = currentsheet.Cells(i, 6).Value
+                    currentsheet.Cells(ticker_index, 10).Value = close_val - open_val
+                    
                     ticker_index = ticker_index + 1
                     i = i -1 'this is needed because the <Next i> will ommit the first value of the new Ticker if not added.
                         ' I figure this out by making a Pivot Table of the original data and compairing that to the info obtained by this script.
