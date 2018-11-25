1bfb944328ab5458cf0249f9dcb5ba89b2198b1e
Joel Sotelo <joel.pilgrim88@gmail.com>
2018-11-24 18:24:03 -0600

    Added the code to clean the used cells to allow the script to run multiple times without having to clean the file.

--- 02_Moderate_script.vb
+++ 02_Moderate_script.vb
@@ -4,6 +4,7 @@ Sub stock_market_data_analysis()
     Dim CodeNameStr As String
 
     Dim LastRow As Long
+    Dim LastRow_tmp As Long
 
     Dim i As Long
 
@@ -18,6 +19,10 @@ Sub stock_market_data_analysis()
         SheetNameStr = currentsheet.Name
         CodeNameStr = currentsheet.CodeName
 
+        ' To clean the cells that we are going to use in the case this script runs twice
+        LastRow_tmp = currentsheet.Cells(currentsheet.Rows.Count, 9).End(xlUp).Row
+        currentsheet.Range("H1:L" & LastRow_tmp).Clear
+
         currentsheet.Range("I1").Value = "Ticker"
         currentsheet.Range("J1").Value = "Yearly Change"
         currentsheet.Range("K1").Value = "Percent Change"
@@ -25,7 +30,7 @@ Sub stock_market_data_analysis()
 
         LastRow = currentsheet.Cells(currentsheet.Rows.Count, 1).End(xlUp).Row
                                   
-        For i = 2 To LastRow
+        For i = 2 To LastRow+1
             'currentsheet.Cells(i, 7).Value
             
             if IsEmpty(currentsheet.Cells(ticker_index, 9).Value)  then
@@ -49,11 +54,12 @@ Sub stock_market_data_analysis()
                         currentsheet.Cells(ticker_index, 10).NumberFormat="0.000000000"
                     end if
 
-                            if (close_val - open_val) >=0 then
-                                currentsheet.Cells(ticker_index, 10).Interior.ColorIndex = 4
-                            else
-                                currentsheet.Cells(ticker_index, 10).Interior.ColorIndex = 3
-                            end if
+                    if (close_val - open_val) >=0 then
+                        currentsheet.Cells(ticker_index, 10).Interior.ColorIndex = 4
+                    else
+                        currentsheet.Cells(ticker_index, 10).Interior.ColorIndex = 3
+                    end if
+
                     currentsheet.Cells(ticker_index, 10).Value = close_val - open_val
 
                     'percent change
@@ -74,35 +80,6 @@ Sub stock_market_data_analysis()
 
         Next i
         
-        'save the close val of the year (last row of the Ticker) -- unique for last Ticker
-        close_val = currentsheet.Cells(i-1, 6).Value
-
-        'MsgBox "Close " & close_val & " Open " & open_val
-        if (close_val - open_val) >=10 or (close_val - open_val) <= -10 then
-            currentsheet.Cells(ticker_index, 10).NumberFormat="0.00000000"
-        else
-            currentsheet.Cells(ticker_index, 10).NumberFormat="0.000000000"
-        end if
-
-        if (close_val - open_val) >=0 then
-            currentsheet.Cells(ticker_index, 10).Interior.ColorIndex = 4
-        else
-            currentsheet.Cells(ticker_index, 10).Interior.ColorIndex = 3
-        end if
-
-        currentsheet.Cells(ticker_index, 10).Value = close_val - open_val
-
-
-
-        'percent change
-        ' Compound Annual Growth Rate = (ending Balance / Beginning Balance)^(1/#years) - 1
-        if open_val <> 0 then
-            currentsheet.Cells(ticker_index, 11).NumberFormat="0.00%"
-            currentsheet.Cells(ticker_index, 11).Value = (close_val/open_val) -1
-        else
-            currentsheet.Cells(ticker_index, 11).Value = "N/A"
-        end if
-
         currentsheet.Columns("J").EntireColumn.AutoFit
         currentsheet.Columns("K").EntireColumn.AutoFit
         currentsheet.Columns("L").EntireColumn.AutoFit
