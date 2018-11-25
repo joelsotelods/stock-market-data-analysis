ef3327af73302dea0b590d57d33cea58d2e06f33
Joel Sotelo <joel.pilgrim88@gmail.com>
2018-11-24 21:54:18 -0600

    Added the Moderate code to the script for the Hard version

--- 03_Hard_script.vb
+++ 03_Hard_script.vb
@@ -4,6 +4,7 @@ Sub stock_market_data_analysis()
     Dim CodeNameStr As String
 
     Dim LastRow As Long
+    Dim LastRow_tmp As Long
 
     Dim i As Long
 
@@ -18,6 +19,10 @@ Sub stock_market_data_analysis()
         SheetNameStr = currentsheet.Name
         CodeNameStr = currentsheet.CodeName
 
+        ' To clean the cells that we are going to use in the case this script runs twice
+        LastRow_tmp = currentsheet.Cells(currentsheet.Rows.Count, 9).End(xlUp).Row
+        currentsheet.Range("H1:L" & LastRow_tmp).Clear
+
         currentsheet.Range("I1").Value = "Ticker"
         currentsheet.Range("J1").Value = "Yearly Change"
         currentsheet.Range("K1").Value = "Percent Change"
@@ -25,34 +30,48 @@ Sub stock_market_data_analysis()
 
         LastRow = currentsheet.Cells(currentsheet.Rows.Count, 1).End(xlUp).Row
                                   
-        For i = 2 To LastRow
-            'currentsheet.Cells(i, 7).Value
+        For i = 2 To LastRow+1
             
             if IsEmpty(currentsheet.Cells(ticker_index, 9).Value)  then
+
                 currentsheet.Cells(ticker_index, 9).Value = currentsheet.Cells(i, 1).Value
                 currentsheet.Cells(ticker_index, 12).Value = currentsheet.Cells(i, 7).Value
                 
-                'xx
+                'saving the open value
                 open_val = currentsheet.Cells(i, 3).Value
                 'MsgBox "Open " & open_val
             else
                 if currentsheet.Cells(ticker_index, 9).Value = currentsheet.Cells(i, 1).Value then
                     currentsheet.Cells(ticker_index, 12).Value = currentsheet.Cells(ticker_index, 12).Value + currentsheet.Cells(i, 7).Value
                 else
-                    'save the close val of the year (last row of the Ticker)
+                    'saving the close val of the year (last row of the Ticker)
                     close_val = currentsheet.Cells(i-1, 6).Value
 
                     'MsgBox "Close " & close_val & " Open " & open_val
+                    if (close_val - open_val) >=10 or (close_val - open_val) <= -10 then
+                        currentsheet.Cells(ticker_index, 10).NumberFormat="0.00000000"
+                    else
+                        currentsheet.Cells(ticker_index, 10).NumberFormat="0.000000000"
+                    end if
+
+                    'Conditional formmating to set the cell green when positive and red when negative
+                    if (close_val - open_val) >=0 then
+                        currentsheet.Cells(ticker_index, 10).Interior.ColorIndex = 4
+                    else
+                        currentsheet.Cells(ticker_index, 10).Interior.ColorIndex = 3
+                    end if
+
                     currentsheet.Cells(ticker_index, 10).Value = close_val - open_val
 
                     'percent change
+                    ' Compound Annual Growth Rate = (ending Balance / Beginning Balance)^(1/#years) - 1   
                     if open_val <> 0 then
+                        currentsheet.Cells(ticker_index, 11).NumberFormat="0.00%"
                         currentsheet.Cells(ticker_index, 11).Value = (close_val/open_val) -1
                     else
-                        currentsheet.Cells(ticker_index, 11).Value = "N/A"
+                        'when open value is 0:
+                        currentsheet.Cells(ticker_index, 11).Value = "N/A" 
                     end if
-                    
-
 
                     ticker_index = ticker_index + 1
                     i = i -1 'this is needed because the <Next i> will ommit the first value of the new Ticker if not added.
@@ -61,25 +80,14 @@ Sub stock_market_data_analysis()
 
             end if
 
-
         Next i
         
-        'save the close val of the year (last row of the Ticker) -- unique for last Ticker
-        close_val = currentsheet.Cells(i-1, 6).Value
+        'Autofit the column size
+        currentsheet.Columns("J").EntireColumn.AutoFit
+        currentsheet.Columns("K").EntireColumn.AutoFit
+        currentsheet.Columns("L").EntireColumn.AutoFit
 
-        'MsgBox "Close " & close_val & " Open " & open_val
-        currentsheet.Cells(ticker_index, 10).Value = close_val - open_val
-
-        'percent change
-        ' Compound Annual Growth Rate = (ending Balance / Beginning Balance)^(1/#years) - 1
-        if open_val <> 0 then
-            currentsheet.Cells(ticker_index, 11).Value = (close_val/open_val) -1
-        else
-            currentsheet.Cells(ticker_index, 11).Value = "N/A"
-        end if
-
-
-        MsgBox SheetNameStr & " was processed. Rows of the file: " & LastRow
+        'MsgBox SheetNameStr & " was processed. Rows on the sheet: " & LastRow
         ticker_index = 2
 
     Next currentsheet
@@ -93,6 +101,7 @@ Sub stock_market_data_analysis()
 
     LastRow = 0
 
+
     i = 0
 
     Dim index_for_metrics As Long
@@ -104,6 +113,9 @@ Sub stock_market_data_analysis()
 
         SheetNameStr = currentsheet.Name
         CodeNameStr = currentsheet.CodeName
+
+        ' To clean the cells that we are going to use in the case this script runs twice
+        currentsheet.Range("N1:Q10").Clear
 
         currentsheet.Range("P1").Value = "Ticker"
         currentsheet.Range("Q1").Value = "Value"
@@ -175,10 +187,12 @@ Sub stock_market_data_analysis()
         currentsheet.Columns("O").EntireColumn.AutoFit
         currentsheet.Columns("Q").EntireColumn.AutoFit
 
-        MsgBox SheetNameStr & " was processed. Rows of the file: " & LastRow
+        'MsgBox SheetNameStr & " was processed. Rows of the file: " & LastRow
         metric_x = 0
 
     Next currentsheet
+
+    MsgBox "The last Sheet (" & SheetNameStr & ") was processed."
 
 End Sub
 
